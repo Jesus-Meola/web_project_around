5c7c2e74983cfc78d75c8a40904b6e8d8bab9217
Jesus Meola <jesusmeola21@gmail.com>
2024-04-24 00:17:48 +0200

    Creando popup

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const editButton = document.querySelector(".profile__edit-button");
+console.log(editButton);
 
+editButton.addEventListener("click", hacerClick);
+
+function hacerClick() {
+  console.log("Hacer Click");
+}
